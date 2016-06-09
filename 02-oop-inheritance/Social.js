0376a90d06d131415292c8fa95476faaa5b58fa4
NicolasLaverde <jnlaverdep@unal.edu.co>
2016-06-08 23:44:44 -0500

    Corrections

---
+++ 02-oop-inheritance/Social.js
@@ -0,0 +1,10 @@
+let Social ={
+	share: function(friendName)
+	{
+		console.log(`${friendName} share  ${this.tittle}`);
+	},
+	like: function(friendName)
+	{
+		console.log(`${friendName} likes ${this.tittle}`);
+	}
+}
